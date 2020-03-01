9e8318f52cc9d06741c3eda41d0e2a4817d7c211
Cher Boon <simcherboon123@gmail.com>
2020-03-01 16:09:37 +0800

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # GoFindGit
+
+Inspired by https://github.com/internetwache/GitTools/blob/master/Finder/gitfinder.py, I decided to write this in Go.
+
+# Usage
+Build the binary
+```go build -o gofindgit main.go```
+
+Run the binary
+```./gofindgit <domains_text_file_name>```
+
+
+## Things to do
+To do:
+- [ ] Add limit to number of go routines
+- [ ] Add functionality to dump git files
+- [ ] Add additional support for other requests (just a random thought)
+
+
+# Support?
+I am also open to contributions, suggestions and feedback! 
--- main.go
+++ main.go
@@ -1,1 +1,80 @@
-main.go
+package main
+
+import (
+	"net/http"
+	"fmt"
+	"time"
+	"bytes"
+	"strings"
+	"bufio"
+	"os"
+	"sync"
+	"crypto/tls"
+)
+var wg sync.WaitGroup
+func RunIt(domain string){
+	target := "http://"+domain+"/.git/HEAD"
+	targetClient := http.Client{Timeout: 2 * time.Second}
+	targetResp, err := targetClient.Get(target)
+	if err != nil{
+		return
+	}
+	if targetResp.StatusCode != 200 {
+		return	
+	}
+	buf := new(bytes.Buffer)
+    buf.ReadFrom(targetResp.Body)
+    newStr := buf.String()
+	// fmt.Println(strings.Contains(newStr, "refs/heads"))
+	if strings.Contains(newStr, "refs/heads") {
+		fmt.Println(domain)
+	}
+	defer wg.Done()
+}
+
+func SecureRunIt(domain string){
+	target := "https://"+domain+"/.git/HEAD"
+	http.DefaultTransport.(*http.Transport).TLSClientConfig = &tls.Config{InsecureSkipVerify: true}
+	targetClient := http.Client{Timeout: 2 * time.Second}
+	targetResp, err := targetClient.Get(target)
+	if err != nil{
+		return
+	}
+	if targetResp.StatusCode != 200 {
+		return	
+	}
+	buf := new(bytes.Buffer)
+    buf.ReadFrom(targetResp.Body)
+    newStr := buf.String()
+	// fmt.Println(strings.Contains(newStr, "refs/heads"))
+	if strings.Contains(newStr, "refs/heads") {
+		fmt.Println(domain)
+	}
+	defer wg.Done()
+}
+
+func main(){
+	argsWithoutProg := os.Args[1:]
+	if len(argsWithoutProg) < 1 {
+		fmt.Println("Usage: gofindgit <domains_text_file_name>")
+		return
+	}
+	file, err := os.Open(os.Args[1])
+    if err != nil {
+        fmt.Println("File does not exist")
+    }
+    defer file.Close()
+
+
+    scanner := bufio.NewScanner(file)
+
+    for scanner.Scan() {             
+		wg.Add(1)
+		go RunIt(scanner.Text())
+		wg.Add(1)
+		go SecureRunIt(scanner.Text())
+		//fmt.Println(scanner.Text())  // token in unicode-char
+
+	}
+	wg.Wait()
+}
